d09b4fb7efcde2fd761eacf28736b842d1a37104
Leandro Estevez <leoest.dev@gmail.com>
2021-08-02 22:10:26 -0400

    Update http server to current needs

--- main.go
+++ main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"errors"
-	"html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"regexp"
+	"text/template"
 )
 
 type Page struct {
@@ -14,67 +12,20 @@ type Page struct {
 	Body  []byte
 }
 
-// Pointers
-// receiver
-// interfaces
-
-var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
-var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+var templates = template.Must(template.ParseFiles("index.html"))
 
 func main() {
-	http.HandleFunc("/view/", viewHandler)
-	http.HandleFunc("/edit/", editHandler)
-	http.HandleFunc("/save/", saveHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/", rootHandler)
+	log.Fatal(http.ListenAndServe(":9000", nil))
 }
 
-func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
-	m := validPath.FindStringSubmatch(r.URL.Path)
-	if m == nil {
-		http.NotFound(w, r)
-		return "", errors.New("invalid Page Title")
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	p, err := loadPage("index")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
-	return m[2], nil // The title is the second subexpression.
-}
-
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-title, err := getTitle(w, r)
-    if err != nil {
-             return
-    }
-    p, err := loadPage(title)
-    if err != nil {
-        http.Redirect(w, r, "/edit/"+title, http.StatusFound)
-        return
-    }
-    renderTemplate(w, "view", p)
-}
-
-func editHandler(w http.ResponseWriter, r *http.Request) {
-	title, err := getTitle(w, r)
-    if err != nil {
-        return
-    }
-    p, err := loadPage(title)
-    if err != nil {
-        p = &Page{Title: title}
-    }
-    renderTemplate(w, "edit", p)
-}
-
-func saveHandler(w http.ResponseWriter, r *http.Request) {
-	title, err := getTitle(w, r)
-    if err != nil {
-        return
-    }
-    body := r.FormValue("body")
-    p := &Page{Title: title, Body: []byte(body)}
-    err = p.save()
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-    http.Redirect(w, r, "/view/"+title, http.StatusFound)
+	renderTemplate(w, "index", p)
 }
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
@@ -84,13 +35,8 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 	}
 }
 
-func (p *Page) save() error {
-	filename := p.Title + ".txt"
-	return ioutil.WriteFile(filename, p.Body, 0600)
-}
-
 func loadPage(title string) (*Page, error) {
-	filename := title + ".txt"
+	filename := title + ".html"
 	body, err := ioutil.ReadFile(filename)
 	if err != nil {
     return nil, err
